95c095b06c43bd6cbf92a0e4e95dba231221a356
isaac <isaacdura@gmail.com>
2015-09-23 17:00:49 +0200

    Test Update1 LeanRoute

---
+++ src/main/java/interfaces/GreetingService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+
+
+import javax.ws.rs.GET;
+
+
+
+/**
+ *
+ * @author isaac
+ */
+public interface GreetingService {
+    
+  @GET("/greeting")
+  def void getGreeting(Callback<Greeting> callback)
+
+    
+}
